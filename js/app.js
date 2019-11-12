4d2b4522ca174864b1ba8008566cc8715f508524
rihab <rihabek>
2019-11-12 21:40:19 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -7,3 +7,34 @@ for (var i = 0;  i < elements.length; i++) {
     element.scrollIntoView({behavior: "smooth"})
   })
 }
+$( document ).ready(function() {
+  $("#cv").click(function(){
+    $(this).css('color','#961108');
+    return false;
+  });
+  $(this).closest(".dropdown").find(".dropdown-toggle").dropdown("toggle");
+
+
+  $('.navbar-nav').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  })
+
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("#menu");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
